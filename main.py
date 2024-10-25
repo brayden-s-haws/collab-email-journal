2472b23847c78b0fda4adde05253973c8d2dae68
brayden-s-haws <58832489+brayden-s-haws@users.noreply.github.com>
2024-10-25 02:03:56 +0000

    added flow to build and send email. started stubbing out main which will orchestrate the flows

--- main.py
+++ main.py
@@ -3,3 +3,16 @@
 # TODO: Setup LLM Flow
 # TODO: Setup LLM prompt with previous questions from database
 # TODO: Setup Orchestration (grab previous questions from database, write new question to database, build email, send email, grab responses from email service)
+
+SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
+sendgrid_list = ''
+
+# new_question = get_claude_question(formatted_prompt)
+# TODO: How to get question_temo_id
+# send_email(SENDGRID_API_KEY, sendgrid_list, new_question)
+# (Wrap these in a cron)
+
+
+# grab email responses
+# (Wrap this in a seperate cron)
+
